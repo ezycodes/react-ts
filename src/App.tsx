f792ad20982436676c24cc13edf70e54f7416ab7
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2024-02-02 01:17:20 +0800

    add tailwind, path alias and inputfield

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,14 @@
 import './App.css'
+import InputField from '@/components/InputField'
 
-function App() {
-  let name: string;
-  let age: number | string;
-  let isStudent: boolean;
-  let hobbies: string[];
-  let role: [number, string];
-
-
-  type PersonProps = {
-    name: string;
-    age: number;
-  }
-
-  let person: Object = {
-    name: "ez",
-    age: 29
-  }
-
-  let lotsOfPeople: PersonProps[];
-
-  let printName: (name: string) => void; // returns undefined
-  let printName1: (name: string) => never; // return nothing / doest not return anything 
-
-  // function printName(name: string) {
-  //   console.log(name);
-  // }
-
-  let personName: unknown;
-
-
-  type X = {
-    a: string;
-    b: number;
-  }
-
-  type Y = X & {
-    c: string;
-    d: number;
-  }
-
-  let y: Y = {
-    c: "efdas",
-    d: 42,
-  }
-
-
-  interface Person {
-    name: string;
-    age?: number;
-  }
-
-  interface Guy extends Person {
-    profession: string;
-  }
-
-  console.log(x)
-
+const App: React.FC = () => {
   return (
-    <>
-      <p className="read-the-docs">
-        Hello World!
-      </p>
-    </>
+    <div className='App'>
+      <span className='heading'>Taskify</span>
+      
+
+      <InputField />
+    </div>
   )
 }
 
