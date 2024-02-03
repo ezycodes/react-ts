7032d5683eae22a7d3d100e7b511a56d11133f44
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2024-02-03 09:31:52 +0800

    done, delete, edit

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,6 @@
 import { TodoProps } from "@/types/types"
-import { Check, Delete, Edit, Trash, Trash2 } from "lucide-react";
+import { Check, Edit, Trash2 } from "lucide-react";
+import { useState } from "react";
 
 interface SingleTodoProps {
   todo: TodoProps;
@@ -7,21 +8,61 @@ interface SingleTodoProps {
   setTodos: React.Dispatch<React.SetStateAction<TodoProps[]>>
 }
 
-const SingleTodo: React.FC<SingleTodoProps> = ({ todo, todos, setTodos }) => {
+const SingleTodo = ({ todo, todos, setTodos }: SingleTodoProps) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(todos.map((todo) =>
+      todo.id === id ? { ...todo, isDone: !todo.isDone } : todo))
+  }
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    )
+    setEdit(false)
+  }
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
   return (
-    <form action="" className="todos__single">
-      <span className="todos__single--text">{todo.todo}</span>
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)} >
+      {edit ? (
+        <input value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="input__box text-slate-900" />
+      ) : todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
+
 
       <div className="flex justify-end items-center flex-row gap-2">
-        <button className="p-1 bg-blue-600 hover:bg-blue-700">
+        <button onClick={() => {
+          if (!edit && !todo.isDone) { setEdit((prev) => !prev); console.log(edit) }
+        }}
+          className="p-1 bg-blue-600 hover:bg-blue-700">
           <Edit />
         </button>
 
-        <button className="p-1 bg-blue-600 hover:bg-blue-700">
+        <button onClick={(e) => {
+          e.preventDefault()
+          handleDelete(todo.id)
+        }}
+          className="p-1 bg-blue-600 hover:bg-blue-700">
           <Trash2 />
         </button>
 
-        <button className="p-1 bg-blue-600 hover:bg-blue-700">
+        <button onClick={(e) => {
+          e.preventDefault()
+          handleDone(todo.id)
+        }}
+          className="p-1 bg-blue-600 hover:bg-blue-700">
           <Check />
         </button>
 
